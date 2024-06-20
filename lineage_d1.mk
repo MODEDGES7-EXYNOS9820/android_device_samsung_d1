905741851417c209accdc00c69513922a21496b6
MODEDGESS7 <145202231+MODEDGES24@users.noreply.github.com>
2024-06-20 11:10:58 +0100

    "d1: bringup for Sakura"

--- lineage_d1.mk
+++ lineage_d1.mk
@@ -39,6 +39,11 @@ TARGET_SCREEN_WIDTH := 1080
 ## Inherit some common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+## Sakura Flags
+SAKURA_MAINTAINER := MODEDGES
+TARGET_BOOT_ANIMATION_RES := 1080
+SAKURA_BUILD_TYPE := gapps
+
 ## Device identifier, this must come after all inclusions
 PRODUCT_NAME := lineage_d1
 PRODUCT_DEVICE := d1
